5dda84098a202e5ca51d61fe61c9d72c03bea24b
ashuashenafi <ashuashenafi@gmail.com>
2020-04-17 16:05:23 -0400

    second commit

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 /*
 This program reverses a string
  */
 public class ReverseString {
     public static void main(String[] args) {
-        String str = "Program";
+        //instantiate scanner object
+        Scanner input = new Scanner(System.in);
+
+        //receive string from user
+        System.out.print("Please enter a string: ");
+        String str = input.nextLine();
+
+        //Reverse
+        System.out.print("\nThe Reversed string: ");
         for(int i = str.length()-1; i >= 0; i--){
 
             System.out.print( str.charAt(i));
